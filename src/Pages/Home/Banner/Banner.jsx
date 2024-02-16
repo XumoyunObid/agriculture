225c78064343448ed2eda6e9044c096b13556685
Xumoyun <obidjonov06122005@gmail.com>
2024-02-16 15:46:52 +0500

    adsfgfcx

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,20 +1,27 @@
-import NextButton from "../../../UI/Button/NextButton"
-import Button from './../../../UI/Button/Button';
+import NextButton from "../../../UI/Button/NextButton";
+import Button from "./../../../UI/Button/Button";
+import bannerBg from "../../../assets/Home/bannerBg.png";
 
 const Banner = () => {
   return (
-    <div className="bg-homebg bg-cover h-[100vh] w-full">
-        <div className="container px-[50px] flex flex-col items-start justify-center pt-[8%]">
-          <h2 className="text-4xl text-secondary italic font-secondary">100% Natural Food</h2>
-          <h1 className="text-[70px] text-primary font-extrabold">
-            Choose the best <br /> healthier way <br /> of life
-          </h1>
-          <Button variant="secondary" className="flex items-center gap-[15px]">
-            Explore Now <NextButton />
-          </Button>
-        </div>
+    <div
+      className="h-[100vh] w-full relative"
+      style={{ backgroundPosition: "center", backgroundSize: "cover" }}
+    >
+      <img src={bannerBg} className="absolute" alt="" />
+      <div className="container px-[50px] flex flex-col items-start justify-center pt-[8%] absolute left-[5%]">
+        <h2 className="text-4xl text-secondary italic font-secondary">
+          100% Natural Food
+        </h2>
+        <h1 className="text-[70px] text-primary font-extrabold">
+          Choose the best <br /> healthier way <br /> of life
+        </h1>
+        <Button variant="secondary" className="flex items-center gap-[15px]">
+          Explore Now <NextButton />
+        </Button>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
