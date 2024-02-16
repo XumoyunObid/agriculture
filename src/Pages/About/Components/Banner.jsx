c0565c62fa62b04c0ac8f9d41b9acd5d5dfef9d2
Xumoyun <obidjonov06122005@gmail.com>
2024-02-16 15:30:57 +0500

    yuiokjhb

--- src/Pages/About/Components/Banner.jsx
+++ src/Pages/About/Components/Banner.jsx
@@ -1,8 +1,12 @@
-import bannerBg from "../../../assets/Home/aboutBg.png"
+import bannerBg from "../../../assets/Home/aboutBg.png";
 
 const AboutBanner = () => {
   return (
-    <div className="h-[50vh] w-full flex items-center justify-center" style={{ backgroundImage: `url(${bannerBg})`, backgroundPosition: "center", backgroundSize: 'cover' }}>
+    <div
+      className="h-[50vh] w-full relative flex items-center justify-center"
+      style={{ backgroundPosition: "center", backgroundSize: "cover" }}
+    >
+      <img src={bannerBg} className="absolute" alt="" />
       <h1 className="text-primary text-[50px] font-extrabold">About Us</h1>
     </div>
   );
