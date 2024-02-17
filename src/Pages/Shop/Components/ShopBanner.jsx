a36d3a9072cd2bdad85971566e934ca6b04751ac
Xumoyun <obidjonov06122005@gmail.com>
2024-02-18 01:35:14 +0500

    done

--- src/Pages/Shop/Components/ShopBanner.jsx
+++ src/Pages/Shop/Components/ShopBanner.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import bg from "../../../assets/Home/shopBg.png";
 
 const ShopBanner = () => {
   return (
-    <div className="h-[50vh] w-full bg-shopbg bg-cover bg-center bg-no-repeat flex items-center justify-center">
+    <div
+      className="h-[50vh] w-full bg-cover bg-center bg-no-repeat flex items-center justify-center"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <h1 className="text-primary text-[50px] font-extrabold">Shop</h1>
     </div>
-  )
-}
+  );
+};
 
-export default ShopBanner
+export default ShopBanner;
--- src/Pages/Shop/SingleProducts/SingleProductBanner.jsx
+++ src/Pages/Shop/SingleProducts/SingleProductBanner.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import bg from "../../../assets/Home/singleshopBg.jpg"
 
 const SingleProductBanner = () => {
   return (
-    <div className="h-[50vh] w-full bg-singleshopbg bg-cover bg-center bg-no-repeat flex items-center justify-center">
+    <div className="h-[50vh] w-full bg-cover bg-center bg-no-repeat flex items-center justify-center" style={{
+      backgroundImage: `url(${bg})`,
+      backgroundRepeat: "no-repeat",
+    }}>
       <h1 className="text-primary text-[50px] font-extrabold">Shop Single</h1>
     </div>
   )
